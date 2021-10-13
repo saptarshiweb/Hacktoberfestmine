1bf8f8331fad567f302af9305eb11be2a039d8fd
judahsemi <31461997+judahsemi@users.noreply.github.com>
2021-10-13 05:51:10 +0100

    Update Sum of natural no. Using python.py

--- Sum of natural no. Using python.py
+++ Sum of natural no. Using python.py
@@ -3,11 +3,13 @@
 num = 16
 
 if num < 0:
-   print("Enter a positive number")
+    print("Enter a positive number")
+elif not isinstance(num, int):
+    print("Enter an integer")
 else:
-   sum = 0
-   # use while loop to iterate until zero
-   while(num > 0):
-       sum += num
-       num -= 1
-   print("The sum is", sum)
+    sum = 0
+    # use while loop to iterate until zero
+    while(num > 0):
+        sum += num
+        num -= 1
+    print("The sum is", sum)
